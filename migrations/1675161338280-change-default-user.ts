e74c3cf5f4067dae85c5fe7710526b0b46d15957
Talerchik-Aleksey <talerchik@protonmail.com>
2023-01-31 13:40:47 +0300

    Add query values

--- migrations/1675161338280-change-default-user.ts
+++ migrations/1675161338280-change-default-user.ts
@@ -1,9 +1,13 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
+const TABLE_NAME = "users";
+const COLUMN_NAME = "is_enabled";
+const DEFAULT_VALUE = "false";
+
 export class ChangeDefaultValue1675161338280 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `ALTER TABLE table_name ALTER COLUMN column_name SET DEFAULT new_default_value`
+      `ALTER TABLE ${TABLE_NAME} ALTER COLUMN ${COLUMN_NAME} SET DEFAULT ${DEFAULT_VALUE}`
     );
   }
 
