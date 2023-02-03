6d2fcd389388f8bed7385a144e3ccafe613c04d0
Dmitry Karpekin <dkarpekin@twelvedevs.com>
2023-02-03 13:04:38 +0300

    added by posts

--- migrations/1675413184439-add-columns-lat-lng.ts
+++ migrations/1675413184439-add-columns-lat-lng.ts
@@ -1,13 +1,14 @@
 import { MigrationInterface, QueryRunner, TableColumn } from "typeorm";
 
-const TABLE_NAME = "users";
+const TABLE_NAME_USERS = "users";
+const TABLE_NAME_POSTS = "posts";
 const COLUMN_NAME_LAT = "lat";
 const COLUMN_NAME_LNG = "lng";
 
 export class addColumnsLatLng1675413184439 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.addColumn(
-      TABLE_NAME,
+      TABLE_NAME_USERS,
       new TableColumn({
         name: COLUMN_NAME_LAT,
         type: "decimal",
@@ -17,7 +18,27 @@ export class addColumnsLatLng1675413184439 implements MigrationInterface {
       })
     );
     await queryRunner.addColumn(
-      TABLE_NAME,
+      TABLE_NAME_USERS,
+      new TableColumn({
+        name: COLUMN_NAME_LNG,
+        type: "decimal",
+        precision: 9,
+        scale: 6,
+        default: null,
+      })
+    );
+    await queryRunner.addColumn(
+      TABLE_NAME_POSTS,
+      new TableColumn({
+        name: COLUMN_NAME_LAT,
+        type: "decimal",
+        precision: 8,
+        scale: 6,
+        default: null,
+      })
+    );
+    await queryRunner.addColumn(
+      TABLE_NAME_POSTS,
       new TableColumn({
         name: COLUMN_NAME_LNG,
         type: "decimal",
@@ -29,7 +50,9 @@ export class addColumnsLatLng1675413184439 implements MigrationInterface {
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropColumn(TABLE_NAME, COLUMN_NAME_LAT);
-    await queryRunner.dropColumn(TABLE_NAME, COLUMN_NAME_LNG);
+    await queryRunner.dropColumn(TABLE_NAME_USERS, COLUMN_NAME_LAT);
+    await queryRunner.dropColumn(TABLE_NAME_USERS, COLUMN_NAME_LNG);
+    await queryRunner.dropColumn(TABLE_NAME_POSTS, COLUMN_NAME_LAT);
+    await queryRunner.dropColumn(TABLE_NAME_POSTS, COLUMN_NAME_LNG);
   }
 }
